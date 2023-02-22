f587fa659b895ae1d878684075e413ba0c381801
OscarTadeo <42451057+OscarTadeo@users.noreply.github.com>
2023-02-21 19:13:28 -0600

    Ejemplo de render triangulo multicolor touch

---
+++ TrianguloColores/app/src/main/java/com/example/triangulocolores/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.triangulocolores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private GLSurfaceView gLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+    }
+}
