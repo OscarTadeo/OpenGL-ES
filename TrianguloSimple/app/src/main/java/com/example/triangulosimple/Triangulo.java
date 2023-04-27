45aa9948597b2be97675de6d90672c73a3038ddf
OscarTadeo <42451057+OscarTadeo@users.noreply.github.com>
2023-04-26 21:26:08 -0600

    Comentarios en español

--- TrianguloSimple/app/src/main/java/com/example/triangulosimple/Triangulo.java
+++ TrianguloSimple/app/src/main/java/com/example/triangulosimple/Triangulo.java
@@ -19,7 +19,6 @@ public class Triangulo {
     private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
 
-
     private final String vertexShaderCode =
             "attribute vec4 vPosition;" +
                     "void main() {" +
@@ -35,13 +34,13 @@ public class Triangulo {
 
     // Definimos las coordenadas de los vertices
     static final int COORDS_PER_VERTEX = 3;
-    static float triangleCoords[] = { // in counterclockwise order:
+    static float triangleCoords[] = {
 
-                0.0f, 0.0f, 0.0f, // Arriba
+            0.0f, 0.0f, 0.0f, // Arriba
 
-                -1.0f, -1.0f, 0.0f, // bottom left
+            -1.0f, -1.0f, 0.0f, // Izquierda
 
-                1.0f, -1.0f, 0.0f // bottom right
+            1.0f, -1.0f, 0.0f // Derecha
 
     };
 
@@ -50,65 +49,65 @@ public class Triangulo {
     float color[] = { 0.5f, 1.0f, 0.5f, 1.0f };
 
     public Triangulo() {
-        // initialize vertex byte buffer for shape coordinates
+        // Inicializa el vertex byte buffer para las coordenadas de la figura.
         ByteBuffer bb = ByteBuffer.allocateDirect(
 
-        // (number of coordinate values * 4 bytes per float)
+                // (cantidad de  valores de las coordenadas * 4 bytes por flotante).
                 triangleCoords.length * 4);
 
-        // use the device hardware’s native byte order
+        // Usar el orden de bytes nativo del hardware del dispositivo.
         bb.order(ByteOrder.nativeOrder());
 
-        // create a floating point buffer from the ByteBuffer
+        // Crear un búfer de punto flotante desde el ByteBuffer.
         vertexBuffer = bb.asFloatBuffer();
 
-        // add the coordinates to the FloatBuffer
+        // Agrega las coordenadas al FloatBuffer.
         vertexBuffer.put(triangleCoords);
 
-        // set the buffer to read the first coordinate
+        // Configurar el búfer para leer la primera coordenada.
         vertexBuffer.position(0);
 
         int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
         int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
 
-        // create empty OpenGL ES Program
+        // Crea un programa OpenGL ES vacío.
         mProgram = GLES20.glCreateProgram();
 
-        // add the vertex shader to program
+        // Agrega el vertex shader al programa.
         GLES20.glAttachShader(mProgram, vertexShader);
 
-        // add the fragment shader to program
+        // Agrega el fragment shader al programa.
         GLES20.glAttachShader(mProgram, fragmentShader);
 
-        // creates OpenGL ES program executables
+        // Crea un programa OpenGL ES ejecutable.
         GLES20.glLinkProgram(mProgram);
     }
 
     public void draw() {
-        // Add program to OpenGL ES environment
+        // Agrega el programa al ambiente de OpenGL ES.
         GLES20.glUseProgram(mProgram);
 
-        // get handle to vertex shader's vPosition member
+        // Obtiene el identificador vPosition desde vertex shader.
         positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
 
-        // Enable a handle to the triangle vertices
+        // Habilita un controlador para los vértices del triángulo.
         GLES20.glEnableVertexAttribArray(positionHandle);
 
-        // Prepare the triangle coordinate data
+        // Preparar los datos de coordenadas del triángulo
         GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
                 GLES20.GL_FLOAT, false,
                 vertexStride, vertexBuffer);
 
-        // get handle to fragment shader's vColor member
+        // Obtiene el identificador vColor desde fragment shader.
         colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
 
-        // Set color for drawing the triangle
+        // Asigna el color para dibujar el triangulo.
         GLES20.glUniform4fv(colorHandle, 1, color, 0);
 
-        // Draw the triangle
+        // Dibuja el triangulo.
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
 
-        // Disable vertex array
+        // Desactiva el arreglo de vertices.
         GLES20.glDisableVertexAttribArray(positionHandle);
     }
 }
